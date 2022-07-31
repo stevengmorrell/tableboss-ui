9ae1471261a2c3b882dc30e227fe70bc3f499fb8
Bumbag <42703220+Bumbag@users.noreply.github.com>
2022-07-31 23:59:10 +0100

    basic routing and some MUI testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
+
+import { Button, ButtonGroup } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 // // Your web app's Firebase configuration
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -25,22 +25,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ButtonGroup variant='contained' aria-label='outlined primary button group'>
+      <Button component={RouterLink} to='/creategame'>
+        Create Game
+      </Button>
+      <Button component={RouterLink} to='/overlay'>
+        Overlay
+      </Button>
+      <Button component={RouterLink} to='/managegames'>
+        Manage Games
+      </Button>
+    </ButtonGroup>
   );
 }
 
