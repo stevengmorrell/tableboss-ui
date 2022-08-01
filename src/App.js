d75b5bf72a02c9625d794f1143a772b15b0da5ed
Bumbag <42703220+Bumbag@users.noreply.github.com>
2022-08-01 23:02:42 +0100

    firebase onValue working on Overlay

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
 import { Button, ButtonGroup } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC69UyXu11bwAopdXzYhwp-vZRaKViNovc",
-//   authDomain: "tableboss-ui.firebaseapp.com",
-//   projectId: "tableboss-ui",
-//   storageBucket: "tableboss-ui.appspot.com",
-//   messagingSenderId: "465758735406",
-//   appId: "1:465758735406:web:823090c70227df273b63bf",
-//   measurementId: "G-847JWHBWK9"
-// };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 function App() {
   return (
---
+++ src/base.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// import { getAnalytics } from "firebase/analytics";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC69UyXu11bwAopdXzYhwp-vZRaKViNovc",
+  authDomain: "tableboss-ui.firebaseapp.com",
+  projectId: "tableboss-ui",
+  storageBucket: "tableboss-ui.appspot.com",
+  messagingSenderId: "465758735406",
+  appId: "1:465758735406:web:823090c70227df273b63bf",
+  measurementId: "G-847JWHBWK9",
+  databaseURL:
+    "https://tableboss-ui-default-rtdb.europe-west1.firebasedatabase.app",
+};
+
+// // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+
+const database = getDatabase(app);
+
+export { database };
+
+//   // named export
+// export { firebaseConfig };
+
+// // default export
+// export default base;
