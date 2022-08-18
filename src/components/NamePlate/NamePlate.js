9c0ce0f5f6c0907ef5844e070f08362b2489cf34
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-08-18 14:44:40 +0100

    played around with clippath and polygon, not quite got it as I would like.

--- src/components/NamePlate/NamePlate.js
+++ src/components/NamePlate/NamePlate.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { Box } from "@mui/system";
+import { Grid } from "@mui/material";
 import PropTypes from "prop-types";
 
 const NamePlate = ({ name, army }) => {
   return (
     <Box>
       <Box component="span" sx={{ color: "#FFFFFF" }}>{name}</Box>
-      <Box sx={{ background: "#EDEDED" }}>
-        <span>{army}</span>
-      </Box>
+      <Grid display="flex" justifyContent="center" alignItems="center"
+        sx={{
+           textTransform: "none",
+           backgroundColor: "#8d99ae",
+           borderRadius: "0px",
+           clipPath: "polygon(100% 0, 100% 65%, 90% 100%, 0 100%, 0 35%, 10% 0)",
+           //(0 100%, 65% 100%, 100% 90%, 100% 0, 35% 0, 0 10%)
+           color: "#000000",
+           fontSize: "2",      
+           alignText: "center"
+                     
+        }}         
+       >
+       {army}</Grid>
     </Box>
   );
 };
--- src/pages/Overlay.js
+++ src/pages/Overlay.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { ref, onValue } from "firebase/database";
 import { database } from "../base";
 import { Box } from "@mui/system";
+import { Button, Grid } from "@mui/material";
 
 import NamePlate from "../components/NamePlate/NamePlate";
 import { Container } from '@mui/material';
@@ -25,17 +26,40 @@ const Overlay = () => {
   });
 
   return (
-    <Box
+    <Box 
+    sx={{
+      border: 10,   
+      borderColor:  "#2B2B2D", 
+      height: "100vh"
+     
+    }}>
+      <Box
       sx={{
         background: "#2B2B2D",
         width: "25%",
-        height: "100vh",
+        height: "98vh",
       }}
     >
       <Container>
         <NamePlate name={playerOneName} army={"Adeptus Mechanics"} />
         <NamePlate name={playerTwoName} army={"Salamanders"} />
+        <Grid display="flex" justifyContent="center" alignItems="center"
+        sx={{
+           mt: 10,
+           textTransform: "none",
+           backgroundColor: "#8d99ae",
+           borderRadius: "0px",
+           clipPath: "polygon(100% 0, 100% 65%, 90% 100%, 0 100%, 0 35%, 10% 0)",
+           //(0 100%, 65% 100%, 100% 90%, 100% 0, 35% 0, 0 10%)
+           color: "#000000",
+           fontSize: "2",      
+           alignText: "center"
+                     
+        }}         
+       >
+       Salamanders</Grid>
       </Container>
+      </Box>
     </Box>
   );
 };
