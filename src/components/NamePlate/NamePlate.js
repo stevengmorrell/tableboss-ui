f400e2ec3cbfcb4f3bf0bbefb2408b7078812cef
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-08-18 15:44:06 +0100

    Figured out clip path polygon. Each pari of numbers is a set of coords, x then y. The % refers to how far from the top left you ant th coord to be. You make an object you want to see with the coords.

--- src/components/NamePlate/NamePlate.js
+++ src/components/NamePlate/NamePlate.js
@@ -7,20 +7,21 @@ const NamePlate = ({ name, army }) => {
   return (
     <Box>
       <Box component="span" sx={{ color: "#FFFFFF" }}>{name}</Box>
-      <Grid display="flex" justifyContent="center" alignItems="center"
+      <Box display="flex" justifyContent="center" alignItems="center"
         sx={{
            textTransform: "none",
            backgroundColor: "#8d99ae",
            borderRadius: "0px",
-           clipPath: "polygon(100% 0, 100% 65%, 90% 100%, 0 100%, 0 35%, 10% 0)",
-           //(0 100%, 65% 100%, 100% 90%, 100% 0, 35% 0, 0 10%)
+           clipPath: "polygon(98% 0, 100% 40%, 100% 100%, 0 100%, 0 0)",
+           //"polygon(80% 0, 100% 15%, 100% 100%, 0 100%, 0 0)",
+           //"polygon(100% 0, 100% 65%, 90% 100%, 0 100%, 0 35%, 10% 0)",
            color: "#000000",
            fontSize: "2",      
            alignText: "center"
                      
         }}         
        >
-       {army}</Grid>
+       {army}</Box>
     </Box>
   );
 };
