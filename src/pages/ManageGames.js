df7f72ce524b42a0e587f1e3240dd9304b2362fe
Bumbag <42703220+Bumbag@users.noreply.github.com>
2022-08-18 13:37:47 +0100

    getting started with react hook form and MUI

--- src/pages/ManageGames.js
+++ src/pages/ManageGames.js
@@ -1,26 +1,69 @@
 import { ref, set } from "firebase/database";
 import React, { useEffect, useState } from "react";
 import { database } from "../base";
+import { useForm, Controller } from "react-hook-form";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 
 const ManageGames = () => {
-  const [name, setName] = useState(null);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
 
-  useEffect(() => {
-    set(ref(database, "users/1"), {
-      name: name,
-    });
-  });
+  const onSubmit = (data) => console.log(data);
 
   return (
-    <div>
-      <h1>Manage Games</h1>
-      <input
-        name='name'
-        type='text'
-        onChange={(e) => setName(e.target.value)}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name={"p1Name"}
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextField
+            required
+            onChange={onChange}
+            value={value}
+            label={"Player One Name"}
+          />
+        )}
       />
-    </div>
+      <Controller
+        control={control}
+        name='selection'
+        default=''
+        render={({ field: { onChange, value } }) => (
+          <Select onChange={onChange} value={value} required>
+            <MenuItem value='Adeptus Mechanicus'>Adeptus Mechanicus</MenuItem>
+            <MenuItem value='Salamanders'>Salamanders</MenuItem>
+            <MenuItem value='Grey Wolves'>Grey Wolves</MenuItem>
+            <MenuItem value='Orks'>Orks</MenuItem>
+          </Select>
+        )}
+      />
+      <Button variant='contained' type='submit'>
+        Submit
+      </Button>
+    </form>
   );
 };
 
 export default ManageGames;
+
+// const [name, setName] = useState(null);
+// useEffect(() => {
+//   set(ref(database, "users/1"), {
+//     name: name,
+//   });
+// });
+
+// <div>
+// <h1>Manage Games</h1>
+// <input
+//   name='name'
+//   type='text'
+//   onChange={(e) => setName(e.target.value)}
+// />
+// </div>
