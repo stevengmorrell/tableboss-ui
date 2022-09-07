273485bc757f8a5e29e7d0ad14c3e0d620324636
Bumbag <42703220+Bumbag@users.noreply.github.com>
2022-09-07 14:09:04 +0100

    add alt to enable build

--- src/components/OverlayImageBottom/OverlayImageBottom.js
+++ src/components/OverlayImageBottom/OverlayImageBottom.js
@@ -2,6 +2,6 @@ import React from "react";
 import placeholderImage from "./placeholder-landscape.png";
 
 const OverlayImageBottom = () => {
-  return <img width='100%' src={placeholderImage} />;
+  return <img width='100%' src={placeholderImage} alt='Overlay Placeholder' />;
 };
 export default OverlayImageBottom;
