a8826acbe34961c55586ee7a9dc0a3bb3b369835
Bumbag <42703220+Bumbag@users.noreply.github.com>
2023-01-08 20:42:34 +0000

    create games max set to 5 per user

--- src/pages/CreateGame.js
+++ src/pages/CreateGame.js
@@ -1,27 +1,25 @@
 import { Button } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import { database } from "../firebase";
-import { ref, set, onValue } from "firebase/database";
+import { ref, set, get } from "firebase/database";
 import { v4 as uuidv4 } from "uuid";
 import { UserAuth } from "../context/AuthContext";
 
 const CreateGame = () => {
   const { user } = UserAuth();
 
-  const [currentGames, setCurrentGames] = useState(0);
-
-  onValue(ref(database, `games/${user?.uid}`), (snapshot) => {
-    snapshot && setCurrentGames(Object.keys(snapshot).length);
-  });
-
-  const createNewGame = () => {
+  const createNewGame = async () => {
+    let games;
     const gameUid = uuidv4();
-
-    currentGames < 5
+    await get(ref(database, `games/${user?.uid}`)).then((snapshot) => {
+      games = snapshot.val() ?? {};
+    });
+    const gameCount = Object.keys(games).length;
+    gameCount < 5
       ? set(ref(database, `games/${user?.uid}/${gameUid}`), {
           somegamedata: true,
         })
-      : console.log("Max Games Reached");
+      : console.log("MAX GAMES REACHED");
   };
 
   return (
